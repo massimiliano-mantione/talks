acc1fbeca8abbc07801183f245c7d6025590e8c8
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2015-09-02 20:11:10 +0200

    Added code to GHP,

---
+++ FutureJs2015Transducers/GHP/code/transducers-scratchpad.js
@@ -0,0 +1,176 @@
+// Function to use 'require' inside LT from this code dir
+var load = function(name) {
+  var root = '/SSD/massi/talks/FutureJs2015Transducers/code/node_modules/';
+  return require(root + name);
+};
+
+
+// This code uses ramda's transducer implementation
+var R = load('ramda');
+
+// This is the input we have (like an Apache log):
+var input = [
+  '127.0.0.1 - - [26/Feb/2015 19:25:25] "GET /static/r.js HTTP/1.1"',
+  '127.0.0.5 - - [26/Feb/2015 19:27:35] "GET /blog/ HTTP/1.1" 200 -',
+  '127.0.0.1 - - [28/Feb/2015 16:44:03] "GET / HTTP/1.1" 200 -',
+  '127.0.0.1 - - [28/Feb/2015 16:44:03] "POST / HTTP/1.1" 200 -'];
+var out;
+
+// This is what we want:
+// only get requests to non-static files:
+var expected =
+  '127.0.0.5 visited http://simplectic.com/blog/\n' +
+  '127.0.0.1 visited http://simplectic.com/\n';
+
+
+// Let's implement the parser building blocks
+
+
+// Is line a GET request for something other than static?
+var isGet = R.test(/ "GET \//);
+var notStatic = R.complement(R.test(/ "GET \/static/));
+var isPage = R.allPass([isGet, notStatic]);
+
+out = R.filter(isPage, input)
+;
+
+// => [ '127.0.0.5 - - [26/Feb/2015 19:27:35] "GET /blog/ HTTP/1.1" 200 -',
+//      '127.0.0.1 - - [28/Feb/2015 16:44:03] "GET / HTTP/1.1" 200 -' ]
+
+
+// Transformation:
+// 'log line' -> ['IP', 'GET /url/path']
+var toIpAndRequest = R.pipe(
+      R.match(/^(\S+).+"([^"]+)"/),
+      R.tail);
+out = R.map(toIpAndRequest, [
+  '127.0.0.5 - - [26/Feb/2015 19:27:35] "GET /blog/ HTTP/1.1" 200 -',
+  '127.0.0.1 - - [28/Feb/2015 16:44:03] "GET / HTTP/1.1" 200 -'])
+;
+
+// => [ [ '127.0.0.5', 'GET /blog/ HTTP/1.1' ],
+//      [ '127.0.0.1', 'GET / HTTP/1.1' ] ]
+
+
+// Transformation:
+// 'GET /url/path' -> 'http://simplectic.com/url/path'
+var requestToUrl = R.pipe(
+      R.split(' '),
+      R.slice(1, 2),
+      R.prepend('http://simplectic.com'),
+      R.join(''));
+out = R.map(requestToUrl, [
+  'GET /blog/ HTTP/1.1',
+  'GET / HTTP/1.1'])
+;
+
+// => ['http://simplectic.com/blog/',
+//     'http://simplectic.com/' ]
+
+
+// Transformation:
+// ['IP', 'GET /url/path'] -> ['IP', 'http://simplectic.com/url/path']
+var requestToUrlInPair = R.over(R.lensIndex(1), requestToUrl);
+out = R.map(requestToUrlInPair, [
+  [ '127.0.0.5', 'GET /blog/ HTTP/1.1' ],
+  [ '127.0.0.1', 'GET / HTTP/1.1' ] ])
+;
+// => [ [ '127.0.0.5', 'http://simplectic.com/blog/' ],
+//      [ '127.0.0.1', 'http://simplectic.com/' ] ]
+
+
+// Build the whole log parser as a transducer
+// (it is a composition of the previous blocks)
+var parseLog = R.compose(
+      // filter non-static GET requests
+      R.filter(isPage),
+      // -> ['IP', 'GET /url/path']
+      R.map(toIpAndRequest),
+      // -> ['IP', 'http://simplectic.com/url/path']
+      R.map(requestToUrlInPair),
+      // -> 'IP visited http://simplectic.com/url/path'
+      R.map(R.join(' visited ')),
+      R.map(R.add(R.__, '\n')));
+
+
+// Apply the transducer
+out = R.transduce(parseLog, R.add, '', input);
+out == expected;
+
+// Do it using the utility 'into' function
+out = R.into('', parseLog, input);
+out == expected;
+
+
+
+// Use the original transducer module
+// (instead of the Ramda one)
+var tr = load('transduce');
+out = tr.into('', parseLog, input);
+out == expected;
+
+
+// Back to slides...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Let's use our parser in push context!
+
+
+// The king of push context is the reactive Framework
+
+var Rx = load('rx');
+
+Rx.Observable.fromArray(input)
+  .transduce(parseLog)
+  .reduce(function (s, t) { return s + t; })
+  .subscribe(function (text) { out = text; });
+
+out == expected;
+
+
+
+// Hey, also Kefir works...
+
+var Kefir = load('kefir');
+
+Kefir.sequentially(0, input)
+  .transduce(parseLog)
+  .reduce(function (s, t) { return s + t; })
+  .onValue(function (text) { out = text; });
+
+out == expected;
+
+
+
+// For added fun, let's use nodejs streams
+
+var stringStream = load('string-streamer');
+var split = load('split');
+var transduceStream = load('transduce-stream');
+var StreamDump = load('dump-stream');
+
+var outputStream = new StreamDump();
+
+stringStream(input.join('\n'))
+  .pipe(split())
+  .pipe(transduceStream(parseLog))
+  .pipe(outputStream)
+  .on('finish', function() { out = outputStream.dump(); });
+
+out == expected;
+
