3ccc8a2b74094c0f4a2f8fcc741368de7ba3fe2f
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2015-09-19 22:21:23 +0200

    Initial files for WebNextConf2015 talk.

---
+++ WebNextConf2015/code/index.js
@@ -0,0 +1,22 @@
+import 'babel-core/polyfill';
+
+import React from 'react';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import configureStore from './store/configureStore';
+import 'todomvc-app-css/index.css';
+
+const store = configureStore();
+
+React.render(
+  <div>
+    <Provider store={store}>
+      {() => <App />}
+    </Provider>
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  </div>,
+  document.getElementById('root')
+);
