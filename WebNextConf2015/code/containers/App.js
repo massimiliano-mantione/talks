869eaebfc0dacb63f2b08f837ceff5b06ea59286
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2015-09-23 17:33:16 +0200

    Moved Todos components to their own files.

--- WebNextConf2015/code/containers/App.js
+++ WebNextConf2015/code/containers/App.js
@@ -1,45 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import Header from '../components/Header';
-import MainSection from '../components/MainSection';
-import PolymerHeader from '../components/polymer/Header';
-import PolymerMainSection from '../components/polymer/MainSection';
+import TodosReact from '../components/Todos';
+import TodosWebComponents from '../components/polymer/Todos';
 import * as TodoActions from '../actions/todos';
-
-class TodosReact extends Component {
-  render() {
-    const { todos, actions } = this.props;
-
-    return (
-      <div>
-        <Header addTodo={actions.addTodo} />
-        <MainSection todos={todos} actions={actions} />
-      </div>
-    );
-  }
-}
-TodosReact.propTypes = {
-  todos: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
-};
-
-class TodosWebComponents extends Component {
-  render() {
-    const { todos, actions } = this.props;
-
-    return (
-      <div>
-        <PolymerHeader addTodo={actions.addTodo} />
-        <PolymerMainSection todos={todos} actions={actions} />
-      </div>
-    );
-  }
-};
-TodosWebComponents.propTypes = {
-  todos: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
-};
 
 class App extends Component {
   render() {
