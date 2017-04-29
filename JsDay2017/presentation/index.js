f6022f33f42faba40cd1e1f5a88024fc6297c469
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2017-04-29 14:57:06 +0200

    Style files and an asset.

--- JsDay2017/presentation/index.js
+++ JsDay2017/presentation/index.js
@@ -1,5 +1,5 @@
 // Import React
-import React from "react";
+import React from 'react'
 
 // Import Spectacle Core tags
 import {
@@ -12,51 +12,58 @@ import {
   Quote,
   Slide,
   Text
-} from "spectacle";
+} from 'spectacle'
 
 // Import image preloader util
-import preloader from "spectacle/lib/utils/preloader";
+import preloader from 'spectacle/lib/utils/preloader'
 
 // Import theme
-import createTheme from "spectacle/lib/themes/default";
+import createTheme from 'spectacle/lib/themes/default'
 
 // Require CSS
-require("normalize.css");
-require("spectacle/lib/themes/default/index.css");
-
+require('normalize.css')
+require('spectacle/lib/themes/default/index.css')
 
 const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
-};
+  city: require('../assets/city.jpg'),
+  kat: require('../assets/kat.png'),
+  logo: require('../assets/formidable-logo.svg'),
+  markdown: require('../assets/markdown.png')
+}
 
-preloader(images);
+preloader(images)
 
 const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quartenary: "#CECECE"
+  primary: 'white',
+  secondary: '#1F2022',
+  tertiary: '#03A9FC',
+  quartenary: '#CECECE'
 }, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+  primary: 'Montserrat',
+  secondary: 'Helvetica'
+})
+
+const t = (props, text) => {
+  if (typeof (props) === 'string') {
+    text = props
+    props = {}
+  }
+  return React.createElement('Text', props, text)
+}
 
 export default class Presentation extends React.Component {
-  render() {
+  render () {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
+      <Deck transition={['zoom', 'slide']} transitionDuration={500} theme={theme}>
+        <Slide transition={['zoom']} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             Spectacle Boilerplate
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
+          {t(
+            { margin: '10px 0 0', textColor: 'tertiary', size: 1 },
+            'open the presentation/index.js file to get started!')}
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
+        <Slide transition={['fade']} bgColor="tertiary">
           <Heading size={6} textColor="primary" caps>Typography</Heading>
           <Heading size={1} textColor="secondary">Heading 1</Heading>
           <Heading size={2} textColor="secondary">Heading 2</Heading>
@@ -65,7 +72,7 @@ export default class Presentation extends React.Component {
           <Heading size={5} textColor="secondary">Heading 5</Heading>
           <Text size={6} textColor="secondary">Standard text</Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
           <Heading size={6} textColor="secondary" caps>Standard List</Heading>
           <List>
             <ListItem>Item 1</ListItem>
@@ -74,13 +81,13 @@ export default class Presentation extends React.Component {
             <ListItem>Item 4</ListItem>
           </List>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
           <BlockQuote>
             <Quote>Example Quote</Quote>
             <Cite>Author</Cite>
           </BlockQuote>
         </Slide>
       </Deck>
-    );
+    )
   }
 }
