d779ef70676f8e6bc0cd3379249ffc1bd07f844b
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2017-05-11 07:53:17 +0200

    Added grid c++ demo to the script.

--- JsDay2017/V8/code/grid/demo.sh
+++ JsDay2017/V8/code/grid/demo.sh
@@ -7,10 +7,20 @@ pause
 echo node grid-2-constructor.js
 node grid-2-constructor.js
 pause
-diff -u grid-1-arraysize.js grid-2-constructor.js
+diff -u grid-1-arraysize.js grid-2-constructor.js | less
 pause
 echo node grid-5-fast.js
 node grid-5-fast.js
 pause
-diff -u grid-1-arraysize.js grid-5-fast.js
+diff -u grid-2-constructor.js grid-5-fast.js | less
+pause
+echo g++ -O3 grid.cc -o grid
+g++ -O3 grid.cc -o grid
+pause
+./grid
+pause
+echo emcc -O3 grid.cc -s TOTAL_MEMORY=128MB -o grid.js
+emcc -O3 grid.cc -s TOTAL_MEMORY=128MB -o grid.js
+pause
+node grid.js
 pause
