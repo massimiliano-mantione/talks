23bb3fecf9e9e16656c87d00e3b12276f96d796a
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2018-06-18 20:20:58 +0200

    Initial slides for APIConf.

--- ApiConf2018/presentation/index.js
+++ ApiConf2018/presentation/index.js
@@ -31,11 +31,8 @@ require('normalize.css')
 require('spectacle/lib/themes/default/index.css')
 
 const images = {
-  react: require('../assets/react-logo.svg'),
-  preact: require('../assets/preact-logo.png'),
-  inferno: require('../assets/infernojs-logo.png'),
-  richHarris: require('../assets/rich-harris.jpg'),
-  danAbramov: require('../assets/dan-abramov.jpg')
+  logo: require('../assets/apiconf-logo.png'),
+  wlogo: require('../assets/apiconf-logo-white.png')
 }
 
 preloader(images)
@@ -147,26 +144,20 @@ const codeBlock = (text) => {
 }
 
 const logo = () => {
-  return <Image src={images.react.replace('/', '')} width="20vw"/>
+  return <Image src={images.logo.replace('/', '')} width="20vw"/>
 }
-const preactLogo = () => {
-  return <Image src={images.preact.replace('/', '')} width="20vw"/>
-}
-const infernoLogo = () => {
-  return <Image src={images.inferno.replace('/', '')} width="20vw"/>
-}
-const talkQrCode = () => {
-  return <Image src={images.react.replace('/', '')} width="20vw"/>
+const wlogo = () => {
+  return <Image src={images.wlogo.replace('/', '')} width="20vw"/>
 }
 
 const slides = () => {
   return [
     slideZoom([
-      logo(),
-      titleNoCaps('Verona, Oct 6 2017', false, 6),
-      title('If it quacks like React', false, 4),
-      title('and renders like React', false, 4),
-      title('is it React?', false, 3),
+      wlogo(),
+      titleNoCaps('Torino, Jun 21 2018', false, 6),
+      title('GraphQL', false, 3),
+      title('as an', false, 4),
+      title('API Gateway', false, 3),
       titleNoCaps('Massimiliano Mantione', false, 6),
       titleNoCaps('@M_a_s_s_i', false, 6)
     ]),
@@ -176,260 +167,124 @@ const slides = () => {
       line('The Unity Game Engine'),
       line('The V8 Team in Google'),
       lineBold('Now CTO and Full Stack developer at Hyperfair'),
-      lineEm('(Virtual Reality Platform)')
+      lineEm('(Virtual Reality on the Web)')
     ]),
 
     slide([
-      title('Strange talk title'),
-      lineEm('What should quack like react?'),
-      line('We are talking about React.js alternatives'),
-      line('Specifically, API-compatible implementations'),
-      lineBold('Yes, there are such things!')
-    ]),
-    slide([
-      title('To make it clear'),
-      line('We are not talking about different UI frameworks'),
-      lineEm('(Angular, Ember, Polymer, Knockout...)'),
-      line('And neither react-like virtual-DOM-based frameworks'),
-      lineEm('(Cycle, Vue, Riot, Ractive, Mithril, Maquette...)')
-    ]),
-    
-    slide([
-      title('Actual focus of this talk'),
-      line('Preact'),
-      lineEm('(maybe react-lite)'),
-      line('Inferno'),
-      lineBold('React-fiber!')
-    ]),
-    slide([
-      title('Why these alternatives?'),
-      line('Mostly, React is big'),
-      line('While relatively fast, it can be relatively slow'),
-      lineEm('The community likes experimenting!')
-    ]),
-
-    slideZoom([
-      preactLogo(),
-      title('Preact')
-    ]),
-    slide([
-      title('Preact', false),
-      line('Mostly focused on size'),
-      lineEm('(3.4 Kb core + 3.6Kb compat vs 47.4 Kb)'),
-      line('Clean and understandable code'),
-      lineEm('(can be understood in one evening)'),
-      line('Also focused on speed')
-    ]),
-    slide([
-      title('Lean', false),
-      line('Closer to the DOM'),
-      line('Reconciler diffs against the DOM'),
-      line('Uses the DOM event handlers'),
-      line('Avoids "reinventing the web"'),
-      line('Plays nice with other code')
-    ]),
-    slide([
-      title('Easy to Use', false),
-      line('props, state and context are passed to render()'),
-      line('Use standard HTML attributes like class and for'),
-      line('Works with React DevTools out of the box'),
-    ]),
-    slide([
-      title('What\'s Missing?'),
-      lineBold('PropType Validation'),
-      lineEm('(supported in preact-compat)'),
-      lineBold('React.Children'),
-      lineEm('(supported in preact-compat)'),
-      lineEm('plus, props.children is always an Array'),
-      lineBold('Synthetic Events'),
-      lineEm('(unneeded extra overhead)'),
-    ]),
-    slide([
-      title('What\'s Different?'),
-      line('render() has a third argument: the root node to replace'),
-      lineEm('(otherwise it appends)'),
-      line('No contextTypes and childContextTypes'),
-      lineEm('(children receive context entries from getChildContext)')
-    ]),
-    slide([
-      title('Used in Production'),
-      line('Uber, Lyft, New York Times, Pepsi, Finantial times...'),
-      line('several ecommerce sites...'),
-      line('...and many others!'),
-      lineBold('Mostly wherever the size difference matters'),
-      lineEm('(often for mobile)')
-    ]),
-
-    slideZoom([
-      infernoLogo(),
-      title('Inferno')
-    ]),
-    slide([
-      title('Inferno', false),
-      line('Also smaller than React'),
-      lineEm('(9.1 Kb core + 8.3Kb compat vs 47.4 Kb)'),
-      line('Very clean and modular code'),
-      lineBold('Its focus is different...')
-    ]),
-    slide([
-      title('SPEED'),
-      line('I mean real speed'),
-      line('Written to be fast'),
-      line('Focused on performance'),
-      lineEm('besides...'),
-      lineBold('...did I mention SPEED?')
-    ]),
-    slide([
-      title('How Did He Do It?'),
-      lineEm('(I mean Dominic Gannaway)'),
-      line('Persistency and Cleverness'),
-      line('Good knowledge of JS VMs'),
-      line('Scientific application of this knowledge'),
-      lineBold('try, experiment, measure, retry')
-    ]),
-    slide([
-      title('Inferno vs React'),
-      line('Smaller and faster'),
-      line('Partial system of syntethic events'),
-      line('No string refs (supported in inferno-compat)'),
-      line('Same dev tools (with inferno-devtools)'),
-      lineBold('Lifecycle events on functional components!')
-    ]),
-    slide([
-      title('Inferno vs Preact'),
-      line('Larger but faster'),
-      line('Diffs against the virtual DOM'),
-      line('Synthetic events also help performance'),
-      line('Supports controlled components'),
-      lineEm('Lifecycle events on functional components')
-    ]),
-    slide([
-      title('Inferno is Faster'),
-      lineBold('...but...'),
-      lineBold('HOW MUCH FASTER?')
-    ]),
-    slide([
-      title('Measuring Performance'),
-      lineEm('Three kind of lies'),
-      line('Lies'),
-      line('Damn Lies'),
-      lineBold('And then there\'s Benchmarks')
-    ]),
-    slide([
-      title('Benchmarks are not Lies'),
-      line('Its just that they are unlikely to measure what is relevant to your case'),
-      lineEm('You should measure performance in your application'),
-      lineBold('That said, Inferno does win benchmarks'),
-      // lineEm({size: 3}, 'http://www.stefankrause.net/js-frameworks-benchmark6/webdriver-ts-results/table.html')
-    ]),
-    slide([
-      title('Svelte Creator'),
-      quoteImage('It\'s competitive with Inferno, which is probably the fastest UI framework in the world, for now, because Dominic Gannaway is a wizard', 'Rich Harris', images.richHarris),
-      lineEm('(BTW: Facebook hired Dominic!)')
-    ]),
-    slide([
-      title('And Now Something'),
-      line({caps: true, fit: true, bold: true}, 'Completely Different'),
-      line({caps: true, fit: true, bold: true}, 'Yet Another API-compatible'),
-      line({caps: true, fit: true, bold: true}, 'React reimplementation')
-    ]),
-    slide([
-      title('React'),
-      line({caps: true, fit: true, bold: true}, 'Itself')
-    ]),
-    slide([
-      title('This reimplementation'),
-      lineEm('is called'),
-      line({caps: true, fit: true, bold: true}, 'React 16')
-    ]),
-    slide([
-      title('What happened?'),
-      line('Also Facebook was not fully happy with React'),
-      line('They went for a core rewrite'),
-      lineBold('Sensibly, they put API compatibility as a top priority'),
-      lineEm('(nobody likes breaking working applications)')
-    ]),
-    slide([
-      title('What was Wrong'),
-      line({caps: true, fit: true, bold: true}, 'with React'),
-      line({caps: true, fit: true, bold: true}, 'for Facebook?')
-    ]),
-    slide([
-      title('Neither size nor raw speed'),
-      line('Inferno is faster, but not orders of magnitude faster'),
-      lineEm('...but...'),
-      lineBold('What if you application became orders of magnitudes more complex?')
-    ]),
-    slide([
-      title('Rendering and Reconciling'),
-      line('Take a time proportional to the size of your DOM tree'),
-      line('In React 15 they are synchronous operations'),
-      lineBold('What if they took more than one frame?'),
-      lineEm('Let\'s see a synthetic benchmark')
-    ]),
-    slide([
-      title('The solution'),
-      lineBold('Make reconciliation asynchronous')
-    ]),
-    slide([
-      title('Async Reconciliation'),
-      line('Each "atomic batch" that updates the DOM runs in a "fiber"'),
-      line('React has a "scheduler" that can run fibers one at a time'),
-      line('Rendering the whole Virtual DOM can span several frames')
-    ]),
-    slide([
-      title('Async Reconciliation'),
-      line('Applying a batch of rendering means reconciling its virtual DOM section with the real DOM'),
-      line('The commit of each batch is atomic (it must be)...'),
-      lineBold('...but the whole process is not!'),
-      lineEm('Let\'s see')
-    ]),
-    slide([
-      title('This is still experimental'),
-      line('React 16 uses the fiber-based reconciler'),
-      line('but'),
-      lineBold('the async reconciliation is disabled by default'),
-      lineEm('(React 16 focuses on fibers causing no regressions)')
-    ]),
-    slide([
-      title('Are Preact and Inferno useful?'),
-      quoteImage('Success of Preact and Inferno = success of React. Different constraints and features but knowledge almost entirely transferable.', 'Dan Abramov', images.danAbramov),
-    ]),
-    slide([
-      title('Do you need them?'),
-      lineEm('...well...'),
-      lineBold('Do you have a problem with React?')
-    ]),
-    slide([
-      title('How do you know?'),
-      line({caps: true, fit: true, bold: true}, 'Measure!'),
-      line('load time (bundle size)'),
-      line('render performance')
-    ]),
-    slide([
-      title('Trying Preact or Inferno'),
-      codeBlock(`resolve.alias = {
-  "react": "preact-compat",
-  "react-dom": "preact-compat"
-}`),
-      line('just put this in WebPack config'),
-      lineEm('if you are shy about it...')
+      title('What is GraphQL?'),
+      line('From graphql.org'),
+      lineBold('A query language for your API'),
+      lineEm('Describe your data'),
+      lineEm('Ask for what you want'),
+      lineEm('Get predictable results')
     ]),
 
     slide([
-      title('A final thought', false, 2),
-      lineEm('If frameworks were vehicles...'),
-      lineEm('...and React 15 were a car...'),
-      line('Preact would be a motorbike'),
-      line('Inferno would be a racing car'),
-      line('React fiber would likely be a large bus with the speed and agility of a sports car'),
-      lineBold('Pick according to your needs!')
+      title('Data Description'),
+      codeBlock(`type Project {
+  name: String
+  tagline: String
+  contributors: [User]
+}`)
+    ]),
+    slide([
+      title('A Simple Query'),
+      codeBlock(`{
+  project(name: "GraphQL") {
+    tagline
+  }
+}`)
+    ]),
+    slide([
+      title('Query Result'),
+      codeBlock(`{
+  "project": {
+    "tagline": "A query language for APIs"
+  }
+}`)
+    ]),
+
+    slide([
+      title('OK, but... Why?'),
+      line('Don\'t we already have REST?'),
+      lineBold('Representational State Transfer'),
+      line('\'State Transfer\' == \'Data Transfer\''),
+      lineEm('Isn\'t this enough?')
+    ]),
+
+    slide([
+      title('REST Issues'),
+      line('Overfetching'),
+      line('Underfetching'),
+      line('Latency'),
+      lineEm('(round trips)')
+    ]),
+
+    slide([
+      title('Exact Fetching'),
+      line('[over-under]fetching'),
+      lineBold('REST has a fixed, URL-based resource granularity'),
+      lineEm('Could use query args, but...')
+    ]),
+
+    slide([
+      title('Multi-resource fetching'),
+      line('Usually through pagination...'),
+      line('...on another, "plural" resource...'),
+      lineBold('Never on other resources'),
+      lineEm('(unless you define custom routes)')
+    ]),
+
+    slide([
+      title('REST is Getting a Mess'),
+      line('Query arguments to limit data transfer'),
+      line('Paging "plural" routes for collections'),
+      line('Custom routes for aggregations')
+    ]),
+
+    slide([
+      title('Enter GraphQL'),
+      lineBold('One single "resource": the "graph"'),
+      line('A query language to explore it'),
+      line('A different route for mutations'),
+      lineEm('(CQRS-ready!)')
+    ]),
+
+    slide([
+      title('DEMO TIME')
+    ]),
+
+    slide([
+      title('Lots of Tooling!'),
+      line('Explicit formal API description'),
+      line('API metadata (documentation...)'),
+      line('Runtime query and result validation'),
+      line('Schema stitching'),
+      line('Interactive API explorer (GraphiQL)')
+    ]),
+
+    slide([
+      title('Language support'),
+      line('Javascript'),
+      line('Python'),
+      line('Golang'),
+      line('Rust'),
+      lineEm('JVM-based, Ruby, whatever...')
+    ]),
+
+    slide([
+      title('Should you use it?'),
+      lineBold('Simple vs Complex API'),
+      line('Facebook'),
+      line('Github'),
+      line('Netflix (Falcor)'),
+      lineEm('And you?')
     ]),
 
     slideZoom([
       title('That\'s all...', false, 5),
       lineEm('Thanks, and...'),
-      line({caps: true, fit: true, bold: true}, 'Questions?')
+      line({ caps: true, fit: true, bold: true }, 'Questions?')
     ])
   ]
 }
