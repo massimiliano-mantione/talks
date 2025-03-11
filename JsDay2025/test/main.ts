4af1aa0ab90c9756464546ebfcaa20b02c5c03f5
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2025-03-11 09:20:24 +0100

    Removed test.

--- JsDay2025/test/main.ts
+++
@@ -1,8 +0,0 @@
-import { Effect, Stream } from 'effect';
-
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
-const s = Stream.fromIterable([1, 2, 3, 4, 5]);
-const mapped = s.map(n => n + 1);
